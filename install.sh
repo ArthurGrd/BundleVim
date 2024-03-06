f66c70f84580196d33817df20ea92162e1bed030
Arthur Garraud <arthur.garraud@epita.fr>
2024-03-06 17:40:14 +0100

    a

--- install.sh
+++ install.sh
@@ -1,26 +1,16 @@
 #Plugs
-mkdir -p ~/.config/nvim
-cp -r -n src/nvim/* ~/.config/nvim/
-#Font
-curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/NerdFontsSymbolsOnly.zip
-unzip NerdFontsSymbolsOnly.zip -d Font
-mkdir -p ~/.fonts
-cp Font/*.ttf ~/.fonts/
-fc-cache -f -v ~/.fonts
-rm -rf Font
-rm NerdFontsSymbolsOnly.zip
-    
-mv nvim ~/
+
+mv src/vim/* ~/.vim/
 
 
 #Plugs Suite
-nvim -c ':PlugInstall' -c ':q' -c ':q'
-nvim -c ':MasonInstall rust-analyzer'
+#nvim -c ':PlugInstall' -c ':q' -c ':q'
+#nvim -c ':MasonInstall rust-analyzer'
 
 if [ -n "$BASH_VERSION" ]; then
-    echo "nvim=''" >> ~/.bashrc
-    source ~/.bashrc
-    echo "Alias défini pour Bash."
+    #echo "nvim=''" >> ~/.bashrc
+    #source ~/.bashrc
+    #echo "Alias défini pour Bash."
 # Vérifie si le shell est Zsh
 elif [ -n "$ZSH_VERSION" ]; then
     # Si le shell est Zsh, définit l'alias dans le fichier .zshrc
