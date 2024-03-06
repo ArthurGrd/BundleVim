6ca791df12677751b3f35dc1898b04888a02d007
arthur.garraud <arthur.garraud@epita.fr>
2024-03-06 15:25:25 +0100

    version 1.0

--- install.sh
+++ install.sh
@@ -1,57 +1,33 @@
-if command -v nvim &> /dev/null
-then
-    #Plugs
-    #curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-       #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    cp -r -n vim/* ~/.config/nvim/
-    cp vim/init.vim ~/.config/nvim/init.vim
-    #mkdir -p ~/.config/nvim/autoload
-    #curl -LSso ~/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-    #Font
-    curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/NerdFontsSymbolsOnly.zip
-    unzip NerdFontsSymbolsOnly.zip -d Font
-    mkdir -p ~/.fonts
-    cp Font/*.ttf ~/.fonts/
-    fc-cache -f -v ~/.fonts
-    rm -rf Font
-    rm NerdFontsSymbolsOnly.zip
+#Plugs
+mkdir -p ~/.config/nvim
+cp -r -n src/nvim/* ~/.config/nvim/
+#Font
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/NerdFontsSymbolsOnly.zip
+unzip NerdFontsSymbolsOnly.zip -d Font
+mkdir -p ~/.fonts
+cp Font/*.ttf ~/.fonts/
+fc-cache -f -v ~/.fonts
+rm -rf Font
+rm NerdFontsSymbolsOnly.zip
     
-    #Plugs Suite
-    nvim -c ':PlugInstall' -c ':q' -c ':q'
+mv nvim ~/
 
-    #YCM
-    #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    #~/.local/share/nvim/plugged/YouCompleteMe/install.py
-    #cp -r third_party/ ~/.local/share/nvim/plugged/YouCompleteMe/third_party
-    #cp -r rls/ ~/.local/share/nvim/plugged/YouCompleteMe/third_party/ycmd/third_party/
-    #./install.py --rust-completer
-    #RUSTUP_HOME=~/rusttmp
-    #rustup toolchain install nightly
-    #rustup default nightly
-    #rustup component add rls rust-analysis rust-src
-    #cd $RUSTUP_HOME/toolchains
-    #cd nightly-*/
-    #mv * ~/.local/share/nvim/plugged/YouCompleteMe/third_party/ycmd/third_party/rls/
-    #rm -rf $RUSTUP_HOME
 
-    # Rajouter plugin rust/ sur la version de python
+#Plugs Suite
+nvim -c ':PlugInstall' -c ':q' -c ':q'
+nvim -c ':MasonInstall rust-analyzer'
 
+if [ -n "$BASH_VERSION" ]; then
+    echo "nvim=''" >> ~/.bashrc
+    source ~/.bashrc
+    echo "Alias défini pour Bash."
+# Vérifie si le shell est Zsh
+elif [ -n "$ZSH_VERSION" ]; then
+    # Si le shell est Zsh, définit l'alias dans le fichier .zshrc
+    echo "alias mon_alias='commande'" >> ~/.zshrc
+    source ~/.zshrc
+    echo "Alias défini pour Zsh."
 else
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    cp -r -n vim/* ~/.vim/
-    cp vim/init.vim ~/.vim/init.vim
-    curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/NerdFontsSymbolsOnly.zip
-    unzip NerdFontsSymbolsOnly.zip -d Font
-    mkdir -p ~/.fonts
-    cp Font/*.ttf ~/.fonts/
-    fc-cache -f -v ~/.fonts
-    rm -rf Font
-    rm NerdFontsSymbolsOnly.zip
-    vim -c ':PlugInstall' -c ':q' -c ':q'
-    cd ~/.vim/plugged/YouCompleteMe/
-    ./install.py --rust-completer
-
+    echo "Shell non pris en charge."
 fi
 
